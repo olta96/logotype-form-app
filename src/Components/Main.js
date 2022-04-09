96aee855beb399a657eb7c2d0de7d4461c2bdec5
olof <olof@takman.se>
2022-04-09 13:48:56 +0200

    Testing AWS

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -5,7 +5,7 @@ export default class Main extends Component {
     render() {
         return (
             <div>
-                Hej Linnea!
+                Hej Linnea T!
             </div>
         );
     }
